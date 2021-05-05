6c9897f9d48beefd675bf54e48fa4cfcf6a8f8da
변지훈 <wlgns1588@naver.com>
2021-05-05 15:44:33 +0900

    간다 제발

---
+++ src/main/java/com/clone/line/model/Member.java
@@ -0,0 +1,41 @@
+package com.clone.line.model;
+
+public class Member {
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private String userEmail;
+	private String userAddress;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	
+	
+}
---
+++ src/test/java/com/clone/line/WebCloneSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCloneSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
